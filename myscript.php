746113f9dff0dcad0c5c49842ecebb75b7456d64
DanieleMorello <danielemorello@live.it>
2023-05-03 16:18:13 +0200

    Print first paragraph + lenght and add legnht on sencon paragraph

--- myscript.php
+++ myscript.php
@@ -1,4 +1,6 @@
 <?php
 $paragraph = trim($_POST["paragraph"]);
 $word_to_censor = trim($_POST["word_to_censor"]);
-echo str_replace("$word_to_censor", "***", $paragraph);
+$paragraph_censored = str_replace("$word_to_censor", "***", $paragraph);
+echo $paragraph . strlen($paragraph);
+echo $paragraph_censored . strlen($paragraph_censored);
