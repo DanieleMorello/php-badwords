caa93c6314e2fc30d4094e5df530f6ecfece02b5
DanieleMorello <danielemorello@live.it>
2023-05-03 16:37:28 +0200

    Echo transport on paragraph html

--- myscript.php
+++ myscript.php
@@ -2,5 +2,28 @@
 $paragraph = trim($_POST["paragraph"]);
 $word_to_censor = trim($_POST["word_to_censor"]);
 $paragraph_censored = str_replace("$word_to_censor", "***", $paragraph);
-echo $paragraph . strlen($paragraph);
-echo $paragraph_censored . strlen($paragraph_censored);
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <p>
+    <?php echo $paragraph . strlen($paragraph); ?>
+  </p>
+
+  <p>
+    <?php echo $paragraph_censored . strlen($paragraph_censored); ?>
+  </p>
+</body>
+
+</html>
